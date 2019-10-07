14de7a848d0363c7378a1ff3e84819131a39b3a8
olamat <ola.mattsson@gmail.com>
2019-10-07 16:28:06 +0200

    Using Qualifiers to select implementing class

--- src/main/java/guru/springframework/controllers/GetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/GetterInjectedController.java
@@ -1,8 +1,8 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -17,6 +17,7 @@ public class GetterInjectedController {
     }
 
     @Autowired
+    @Qualifier("getterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }

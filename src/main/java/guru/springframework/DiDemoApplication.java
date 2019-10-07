f58df80a1e19e015dfb3522df9239860233e37f3
olamat <ola.mattsson@gmail.com>
2019-10-07 13:27:56 +0200

    Created simple hello world

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -1,12 +1,16 @@
 package guru.springframework;
 
+import guru.springframework.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		myController.hello();
 	}
 }

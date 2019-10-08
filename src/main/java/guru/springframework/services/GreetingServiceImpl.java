ddb3d62eec853ee1cda146641b1a18c399001dfc
olamat <ola.mattsson@gmail.com>
2019-10-08 10:27:48 +0200

    Reverted back to @Service

--- src/main/java/guru/springframework/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/services/GreetingServiceImpl.java
@@ -1,11 +1,11 @@
 package guru.springframework.services;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
-@Component
+@Service
 public class GreetingServiceImpl implements GreetingService {
 
     public static final String HELLO_GURUS = "Hello Gurus!!!! - Original";
